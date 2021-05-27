564781621e08349321c25e73b7fed4d525bb788e
Stas <stas001w@mail.ru>
2021-05-27 20:33:56 +0300

    improvementJS

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,28 @@
-let navMain = document.querySelector(".main-navigation");
-let navToggle = document.querySelector(".main-navigation__toggle");
+const navigation = () => {
+  let navMain = document.querySelector(".main-navigation");
+  let navToggle = document.querySelector(".main-navigation__toggle");
 
-navMain.classList.remove("main-navigation--nojs");
+  navMain.classList.remove("main-navigation--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains("main-navigation--closed")) {
-    navMain.classList.remove("main-navigation--closed");
-    navMain.classList.add("main-navigation--opened");
-  } else {
-    navMain.classList.add("main-navigation--closed");
-    navMain.classList.remove("main-navigation--opened");
-  }
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains("main-navigation--closed")) {
+      navMain.classList.remove("main-navigation--closed");
+      navMain.classList.add("main-navigation--opened");
+    } else {
+      navMain.classList.add("main-navigation--closed");
+      navMain.classList.remove("main-navigation--opened");
+    }
 });
+}
+
+//При ошибки код выполняется!
+
+let isStorage = navigation;
+let storage = "";
+
+try {
+  storage = navigation();
+}catch (err){
+  isStorage = false;
+  // console.log("Ошибка, код выполняется!");
+}
