6b99bc6e9e3a40f9cfb2ea81b134c75aeb22a1c0
Stas <stas001w@mail.ru>
2021-05-30 17:13:21 +0300

    upgrade

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,47 @@
 const navigation = () => {
-  let navMain = document.querySelector(".main-navigation");
-  let navToggle = document.querySelector(".main-navigation__toggle");
 
-  navMain.classList.remove("main-navigation--nojs");
+  const page = document.querySelector('.page');
+  const navMain = page.querySelector('.main-navigation');
+  const navToggle = navMain.querySelector('.main-navigation__toggle');
+  const navList = navMain.querySelector('.main-navigation__list');
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains("main-navigation--closed")) {
-      navMain.classList.remove("main-navigation--closed");
-      navMain.classList.add("main-navigation--opened");
-    } else {
-      navMain.classList.add("main-navigation--closed");
-      navMain.classList.remove("main-navigation--opened");
+  const openMenu = () => {
+    page.classList.add('page--no-scroll');
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+    navList.addEventListener('click', onMenuClick);
+    document.addEventListener('keydown', onMenuEsc);
+  };
+
+  const closeMenu = () => {
+    page.classList.remove('page--no-scroll');
+    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add('main-navigation--closed');
+    navList.removeEventListener('click', onMenuClick);
+    document.removeEventListener('keydown', onMenuEsc);
+  };
+
+  const onMenuClick = () => {
+    closeMenu();
+  };
+
+  const onMenuEsc = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      closeMenu();
     }
-});
+  };
+
+  navMain.classList.remove('main-navigation--nojs');
+  navMain.classList.add('main-navigation--closed');
+
+  navToggle.addEventListener('click', () => {
+    if (navMain.classList.contains('main-navigation--opened')) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
 }
 
 //При ошибки код выполняется!
@@ -22,6 +51,6 @@ let storage = "";
 
 try {
   storage = navigation();
-}catch (err){
+} catch (err) {
   isStorage = false;
 }
